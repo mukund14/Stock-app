66dce41e78b28de935975ea2cdcf34b2ea9dbb1a
Mukundan Sankar <mukundan.sankar14@gmail.com>
2020-06-20 19:40:27 -0700

    Deploy files

--- streaming.py
+++ streaming.py
@@ -197,7 +197,7 @@ for (k,v) in stocks_dic.items():
         df2['year'] = pd.DatetimeIndex(df2['Date']).year
 
         df2['months']=df2['months'].map({1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'})
-d4=pd.DataFrame(df2.groupby(['year','month','To Grade'])['Firm'].count())
+        d4=pd.DataFrame(df2.groupby(['year','month','To Grade'])['Firm'].count())
         d4=d4.reset_index()
 
         d4.rename(columns={
